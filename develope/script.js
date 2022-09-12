ed5e628948f89fcf69dcf92f8ca849b57edf8f15
Roy <loganroyjr4@gmail.com>
2022-09-12 15:44:27 -0500

    added code to get get weatherAPI() to populate HTML ID's with current data

--- develope/script.js
+++ develope/script.js
@@ -1,44 +1,56 @@
-var userInput = "chicago";
-var apiKey = "18498ae985b8d6f193252af6f0bb056e"
-var requestUrl1 = "https://api.openweathermap.org/data/2.5/weather?q=" + userInput + "&appid=" + apiKey
-var requestUrl2 = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey
 
-var lat = "";
-var lon = "";
-
-function getWeather() {
+function getLatAndLon(userCity) {
+    var apiKey = "18498ae985b8d6f193252af6f0bb056e"
+    var requestUrl1 = "https://api.openweathermap.org/data/2.5/weather?q=" + userCity + "&appid=" + apiKey
+    console.log('req url 1', requestUrl1)
     // first api call to get the lat and lon
     $.ajax({
         url: requestUrl1,
         method: 'GET',
-    }).then(function (response) {
-        console.log('AJAX Response \n-------------');
-        console.log(response);
-        lat = response.coord.lat;
-        lon = response.coord.lon;
+    }).then(function (dataFromFirstAPI) {
 
-        $("#city").text(response.name);
-        $("#date").text(moment.unix(response.dt).format("dddd, MM/DD/YYYY"));
-
-        localStorage.setItem("cityname", response.name);
-
-        // getWeatherApi(lat,lon);
-
+        console.log('AJAX dataFromFirstAPI \n-------------');
+        console.log(dataFromFirstAPI);
+        var lat = dataFromFirstAPI.coord.lat;
+        var lon = dataFromFirstAPI.coord.lon;
+        
+        $("#city").text(dataFromFirstAPI.name);
+        $("#date").text(moment.unix(dataFromFirstAPI.dt).format("dddd, MM/DD/YYYY"));
+        
+        localStorage.setItem("cityname", dataFromFirstAPI.name);
+        
+        getWeatherApi(lat,lon);
+        
     });
 };
 
-// function getWeatherApi(lat,lon){
+function getWeatherApi(lat,lon){
+    var requestUrl2 = "https://api.openweathermap.org/data/3.0/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly&appid=18498ae985b8d6f193252af6f0bb056e&units=imperial";
 
-//     $.ajax({
-//         url: requestUrl2,
-//         method: "GET",
-//     }).then(function(response){
-//         console.log(response);
-//     })
-// };
+    $.ajax({
+        url: requestUrl2,
+        method: "GET",
+    }).then(function(response){
+        console.log(response);
+        // clears previously displayed 5-day forecast
+        $(".card-deck").empty();
+        // gets weather icon and append to page
+        $(".card-deck").empty();
+        var icon = response.current.weather[0].icon;
+        var iconImg = $("<img>");
+        iconImg.attr("src","https://openweathermap.org/img/wn/" + icon + "@2x.png")
+        $("#city").append(iconImg);
+        // populates html ID's with the current weather data
+        $("#temp").text("Temperature: " + response.current.temp + "° F");
+        $("#humidity").text("Humidity: " + response.current.humidity + "%");
+        $("#wind").text("Wind: " + response.current.wind_speed + "mph");
+        // displays the html to user
+        $(".current-data").css({"display":"block"});
+    })
+};
 
 function searchBtn() {
-    userInput = $("input").val().trim();
+    var userInput = $("input").val().trim();
     //buttons created dynamically as the user enters cities 
     var cityList = $("<button>");
     cityList.addClass("previously-searched");
@@ -48,7 +60,7 @@ function searchBtn() {
     // after the user city is saved the input field is cleared
     $("input").val("");
 
-    getWeather();
+    getLatAndLon(userInput);
 };
 
 $("#city-form").submit(function (event) {
